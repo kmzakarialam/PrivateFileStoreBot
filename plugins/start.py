f415594d042723698e8ccdd24aa1354e74d767c7
K. M. Zakaria Alam <149001544+kmzakarialam@users.noreply.github.com>
2023-11-19 03:36:56 +0600

    Add files via upload

--- plugins/start.py
+++ plugins/start.py
@@ -78,12 +78,11 @@ async def start_command(client: Client, message: Message):
             try:
                 z = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
                 await asyncio.sleep(20)
-                await l.delete()
                 except FloodWait as e:
                 await asyncio.sleep(e.x)
-                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                
-                
+                m = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                await m.delete()
+                await z.delete()
             except:
                 pass
         return
